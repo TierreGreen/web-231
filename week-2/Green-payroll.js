b118f762ed43a5bb8f459e6363cd4facd86aef52
Tiérre Green <tigreen@my365.bellevue.edu>
2021-01-17 17:52:35 -0500

    Add files via upload
    "created new assignment 2.3 document and updated landing page, updated global.css file, updated site.css file for week-2 updated global.css file for week-2, created payroll files."

---
+++ week-2/Green-payroll.js
@@ -0,0 +1,87 @@
+/*
+============================================
+; Title:  Green-exercise2.js
+; Author: Tierre Green
+; Date:  17 January 2021
+; Description: Displays message to the console window
+;===========================================
+*/
+
+/**
+============================================
+; Title:  JavaScript Where To
+; Author: w3schools.com
+; Date:  17 January 2021
+; Modified By: Tierre Green
+; Description: calling js statement by using document.getElementById
+;===========================================
+ */
+
+ /**
+  * Employee 1 variables declared and created to bind each data set
+  * to it's individual list item ids' innerHTML.
+  */
+
+let firstName1 = "Ludwig";
+let lastName1 = "Beethoven";
+let address1 = "505 main street";
+let hireDate1 = new Date();
+let locale1 = hireDate1.toLocaleDateString();  
+let payRate1 = 18.9;
+let fixed1 = payRate1.toFixed(1);
+
+/**
+ * calling bound documents to listed items for execution for employee 1.
+*/
+
+document.getElementById("txtFirstNameLud").innerHTML = firstName1;
+document.getElementById("txtLastNameBeet").innerHTML = lastName1;
+document.getElementById("txtAddressLud").innerHTML = address1;
+document.getElementById("txtHireDateLud").innerHTML = locale1;
+document.getElementById("txtPayRateLud").innerHTML = fixed1;
+
+/**
+  * Employee 2 variables declared and created to bind each data set
+  * to it's individual list item ids' innerHTML.
+  */
+
+ let firstName2 = "Johann";
+ let lastName2 = "Bach";
+ let address2 = "512 main street";
+ let hireDate2 = new Date();
+ let locale2 = hireDate2.toLocaleDateString();  
+ let payRate2 = 25.5;
+ let fixed2 = payRate2.toFixed(1);
+
+ /**
+ * Calling bound documents to listed items for execution for employee 2.
+*/
+ 
+ document.getElementById("txtFirstNameJoh").innerHTML = firstName2;
+ document.getElementById("txtLastNameBach").innerHTML = lastName2;
+ document.getElementById("txtAddressJoh").innerHTML = address2;
+ document.getElementById("txtHireDateJoh").innerHTML = locale2;
+ document.getElementById("txtPayRateJoh").innerHTML = fixed2;
+
+ /**
+  * Employee 2 variables declared and created to bind each data set
+  * to it's individual list item ids' innerHTML.
+  */
+
+ let firstName3 = "Wolfgang";
+ let lastName3 = "Mozart";
+ let address3 = "600 main street";
+ let hireDate3 = new Date();
+ let locale3 = hireDate3.toLocaleDateString();  
+ let payRate3 = 50.1;
+ let fixed3 = payRate3.toFixed(1);
+
+ /**
+ * Calling bound documents to listed items for execution for employee 2.
+*/
+ 
+ document.getElementById("txtFirstNameWol").innerHTML = firstName3;
+ document.getElementById("txtLastNameMoza").innerHTML = lastName3;
+ document.getElementById("txtAddressWol").innerHTML = address3;
+ document.getElementById("txtHireDateWol").innerHTML = locale3;
+ document.getElementById("txtPayRateWol").innerHTML = fixed3;
