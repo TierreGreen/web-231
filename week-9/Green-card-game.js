6a106da1f570724708cf1c9f46d35bccc24dfee0
Tiérre Green <tigreen@my365.bellevue.edu>
2021-03-03 05:16:49 -0500

    created new files for Green-card-gam and updated index.html file.

---
+++ week-9/Green-card-game.js
@@ -0,0 +1,10 @@
+/**
+;Title: Green-card-game.js Assignment 9.2
+;Author: Richard Krasso
+;Modified by: Tierre Green
+;Date: March 3, 2021
+;Description: Assignment 9.2 js project script.
+*/
+
+//onclick event to allow button to execute action by dealer.
+document.getElementById("btnDealCards").onclick = function() {myFunction()};
