f64d9521f5eaf246b639ea39c79b0239a40cdc58
Tiérre Green <tigreen@my365.bellevue.edu>
2021-03-04 05:41:15 -0500

    updated Javascript file

--- week-9/Green-card-game.js
+++ week-9/Green-card-game.js
@@ -7,4 +7,47 @@
 */
 
 //onclick event to allow button to execute action by dealer.
-document.getElementById("btnDealCards").onclick = function() {myFunction()};
+document.getElementById("btnDealCards").onclick = function() {getDeckOfCards()};
+
+class Card {
+    constructor(faces, suits) {
+        this.faces = faces;
+        this.suits = suits;
+    }
+}
+
+class Dealer {
+    constructor() {
+
+    //creating a card count variable for the 52 cards in the deck.
+        let CARD_COUNT = 52;
+
+    //creating variable for cares with empty array
+        let cards = [];
+
+    //card deck faces array
+        const faces = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+
+    //suits variabel with all suits Array
+        const suits = ["Hearts", "Diamonds", "Clubs", "Spades"];
+
+    //get deck of cards function
+        function getDeckOfCards() {
+
+        //for loop with 52 iterations. 
+            for (let count = 0; count < this.CARD_COUNT.length; count++) {
+                this.cards[count] = new Card(this.faces[count % 13], this.suits[Math.floor(count / 13)]);
+            }
+        }
+
+    //shuffle function to shuffle the 52 cards
+        function shuffle() {
+            
+            for (let firstCard = 0; firstCard < this.CARD_COUNT.length; firstCard++) {
+
+            }
+
+        }
+    } 
+}
+
